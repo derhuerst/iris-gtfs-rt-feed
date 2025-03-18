582e8fbe2718c86c13f0f98d8a313ea6e8404e00
Jannis R <mail@jannisr.de>
2025-03-18 02:20:08 +0100

    subscribe to Redis & match IRIS items [todo]

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+import {createLogger} from './lib/logger.js'
 import {matchIrisMessages} from './lib/match.js'
+
+const logger = createLogger('service')
 
 try {
 	await matchIrisMessages({
+		logger,
 	})
 } catch (err) {
 	logger.error(err)
